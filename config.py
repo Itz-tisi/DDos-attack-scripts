81cd1dc4f51fe380769ef071e9dc642de7dab6cd
Itz-tisi <b83795814@gmail.com>
2025-01-29 09:24:01 +0530

    Update

---
+++ config.py
@@ -0,0 +1,12 @@
+
+# Don't use quotes( " and ' )
+#SCRIPT BY SASUKE
+
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7683592167:AAHvt7WM91_xpKiGKifYk3dIuNa7jUEMvdI")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("INFLICT_BGMI")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7263607387")
